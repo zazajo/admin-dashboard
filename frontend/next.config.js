8521b5c65ad4e28483f35a910e4b615d873674ea
zazajo <Josephedward201@gmail.com>
2026-03-06 09:43:48 +0100

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,14 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    swcMinify: true,
-    webpack: (config) => {
-      config.resolve.alias = {
-        ...config.resolve.alias,
-        '@': require('path').resolve(__dirname, 'src'),
-      };
-      return config;
-    },
+    turbopack: {},
   };
   
   module.exports = nextConfig;
